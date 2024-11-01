1dd6a615886a1dec140abeea1b815a2940a660c0
MartinYeung5 <luckylucky2010201@yahoo.com.hk>
2024-11-01 21:32:37 +0800

    20241101build1

---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,7 @@
+import { tokenAbi } from "./tokenAbi";
+import { bookingAbi } from "./bookingAbi";
+
+const tokenAddress = "0x3948FbB2e861dB6Ad98532324381C03E760Fb036";
+const bookingAddress = "0x79663C48554A7594435250D3Faa3C93DD8318587";
+
+export { tokenAbi, tokenAddress, bookingAbi, bookingAddress };
